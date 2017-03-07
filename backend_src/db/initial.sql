665f5f09b39c8a6ea84a4789f7240e4d3a2e5fa7
David D'Agostino <davedags@gmail.com>
2017-03-06 23:09:38 -0500

    add user table to initial sql

--- backend_src/db/initial.sql
+++ backend_src/db/initial.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `song` (
+CREATE TABLE if not exists `song` (
   `song_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
@@ -11,3 +11,14 @@ CREATE TABLE `song` (
   KEY `url_key` (`url`),
   FULLTEXT idx (`lyrics`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE if not exists `user` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `username` (`username`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
