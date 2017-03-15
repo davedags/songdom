4cb074924ca6e755d04de430806127379fa47e28
David D'Agostino <dagostin@symplicity.com>
2017-03-14 22:35:02 -0400

    fix db loading

--- backend_src/App.php
+++ backend_src/App.php
@@ -13,8 +13,9 @@ class App
     public function __construct() 
     {
         $config = [];
-        if (file_exists('./config/.env')) {
-            $dotenv = new \Dotenv\Dotenv('./config');
+
+        if (file_exists(__DIR__ . '/config/.env')) {
+            $dotenv = new \Dotenv\Dotenv(__DIR__ . '/config');
             $dotenv->load();
             
             $config['db']['host'] = $_ENV['SONGDOM_DB_HOST'];
@@ -32,7 +33,7 @@ class App
         if (!empty($config['db'])) {
             $container['db'] = function ($c) {
                 $db = $c['settings']['db'];
-                $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
+                $pdo = new \PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
                 $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 $pdo->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
                 return $pdo;
